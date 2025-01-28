33cb66ef96f2bfc0f7267491781db4806efeac4b
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-01-29 01:20:59 +0300

    Add document and post routes with CRUD operations and Swagger documentation for Blog API

---
+++ backend/src/routes/document.js
@@ -0,0 +1,29 @@
+"use strict"
+
+/* --- BLOG API DOCUMENT ROUTE --- */
+
+const router = require('express').Router()
+
+//? URL: /documents
+router.all('/', (req, res) => {
+    res.send({
+        swagger: "documents/swagger",
+        redoc: "documents/redoc",
+        json: "documents/json"
+    })
+})
+
+//? JSON
+router.use('/json', (req, res) => {
+    res.sendFile(`/src/configs/swagger.json`, { root: '.' })
+})
+
+//? REDOC
+const redoc = require('redoc-express')
+router.use('/redoc', redoc.init(`/src/configs/swagger.json`, { title: 'Blog API', pathInMiddlewares: true }))
+
+//? SWAGGER
+const swaggerUi = require('swagger-ui-express')
+router.use('/swagger', swaggerUi.serve, swaggerUi.setup(`/src/configs/swagger.json`))
+
+module.exports = router
