20cb34800fd16b9f6cb7c007559f9e5f0e835bde
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-01-29 01:28:15 +0300

    Add index route for Blog API to organize main routes

---
+++ backend/src/routes/index.js
@@ -0,0 +1,26 @@
+"use strict"
+
+/* --- BLOG API INDEX ROUTE --- */
+
+const router = require('express').Router()
+
+//? URL: /
+
+//? auth
+router.use('/auth', require('./auth'))
+//? user
+router.use('/users', require('./user'))
+//? token
+router.use('/tokens', require('./token'))
+
+//? post
+router.use('/posts', require('./post'))
+//? comment
+router.use('/comments', require('./comment'))
+//? category
+router.use('/categories', require('./category'))
+
+//? document
+router.use('/documents', require('./document'))
+
+module.exports = router
