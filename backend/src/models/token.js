092a7752a06b1e13a2edb6395fe0410142e09187
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-01-28 20:37:50 +0300

    Add error handling and filtering, searching, sorting, and pagination middleware for Blog API

---
+++ backend/src/models/token.js
@@ -0,0 +1,18 @@
+"use strict"
+
+/* --- BLOG API TOKEN --- */
+
+const mongoose = require('mongoose')
+
+// { "userId": "65343222b67e9681f937f001", "token": "...tokenKey..." }
+
+//? Token Model
+const tokenSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+
+    token: { type: String, required: true, index: true, trim: true },
+},
+    { collection: 'tokens', timestamps: true })
+
+//? Token Export
+module.exports = mongoose.model('Token', tokenSchema)
