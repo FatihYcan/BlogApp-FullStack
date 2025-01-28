67f778106152431c080b5f83b2e3e67a55138e14
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-01-28 20:21:47 +0300

    Implement initial setup for Blog API with Express and MongoDB connection

---
+++ backend/index.js
@@ -0,0 +1,62 @@
+"use strict"
+
+/* --- BLOG API --- */
+
+const express = require("express")
+const app = express()
+
+/* ------------------------------------------------------- */
+
+//? envVariables to process.env
+require("dotenv").config()
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+
+//? Aync error handling
+require("express-async-errors")
+
+/* ------------------------------------------------------- */
+
+//? DB connection
+require("./src/configs/dbConnection")
+
+/* ------------------------------------------------------- */
+
+//? Middlewares
+
+//? Accept JSON
+app.use(express.json())
+
+//? Check Authentication
+app.use(require('./src/middlewares/authentication'))
+
+//? res.getModelList()
+app.use(require('./src/middlewares/findSearchSortPage'))
+
+/* ------------------------------------------------------- */
+
+//? Routes
+
+//? Home Path
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to Blog API',
+        documents: {
+            swagger: '/documents/swagger',
+            redoc: '/documents/redoc',
+            json: '/documents/json',
+        },
+    })
+})
+
+//? Routes
+app.use(require('./src/routes'))
+
+/* ------------------------------------------------------- */
+
+//? errorHandler
+app.use(require('./src/middlewares/errorHandler'))
+
+//? RUN SERVER
+app.listen(PORT, HOST, () => { console.log(`Server is running on http://${HOST}:${PORT}`) })
