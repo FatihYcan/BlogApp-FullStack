44d56c67d44dc017e88064c69fe3026917723560
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-01-28 21:07:03 +0300

    Add Category model for Blog API with Mongoose schema

---
+++ backend/src/models/category.js
@@ -0,0 +1,14 @@
+"use strict"
+
+/* --- BLOG API CATEGORY --- */
+
+const mongoose = require('mongoose')
+
+//? Category Model
+const categorySchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true, trim: true },
+},
+    { collection: 'categories', timestamps: true })
+
+//? Category Model Export
+module.exports = mongoose.model('Category', categorySchema)
