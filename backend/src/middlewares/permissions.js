da5d08bd57ac00586254d4e47ecf8157cb438edb
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-01-29 01:06:02 +0300

    Add category routes and permissions middleware for Blog API

---
+++ backend/src/middlewares/permissions.js
@@ -0,0 +1,28 @@
+"use strict"
+
+/* --- BLOG API PERMISSION --- */
+
+module.exports = {
+
+    isLogin: (req, res, next) => {
+        // return next()
+
+        if (req.user && res.user.isActive) {
+            next()
+        } else {
+            res.errorStatusCode = 401
+            throw new Error("NoPermission: You must login")
+        }
+    },
+
+    isAdmin: (req, res, next) => {
+        // return next()
+
+        if (req.user && req.user.isActive && req.user.isAdmin) {
+            next()
+        } else {
+            res.errorStatusCode = 401
+            throw new Error("NoPermission: You must be an admin")
+        }
+    }
+}
--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -3,3 +3,12 @@
 /* --- BLOG API CATEGORY ROUTE --- */
 
 const router = require('express').Router()
+
+const category = require('../controllers/category')
+const permissions = require('../middlewares/permissions')
+
+//? URL: /cetegories
+route.route('/').get(category.list).post(permissions.isAdmin, category.create)
+route.route('/:id').get(category.read).put(permissions.isAdmin, category.update).patch(permissions.isAdmin, category.update).delete(permissions.isAdmin, category.delete)
+
+module.exports = router
