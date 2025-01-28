6ec1dd11790943f8bf85ac226f97f714bdadf844
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-01-28 20:58:19 +0300

    Add password encryption and user model for Blog API

---
+++ backend/src/models/user.js
@@ -0,0 +1,33 @@
+"use strict"
+
+/* --- BLOG API TOKEN --- */
+
+const mongoose = require('mongoose')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+//? User Model
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, trim: true },
+
+    firstName: { type: String, required: true, trim: true },
+
+    lastName: { type: String, required: true, trim: true },
+
+    email: { type: String, required: true, unique: true, trim: true, validate: [(email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email), 'Email type is not correct.'] },
+
+    password: {
+        type: String, required: true, trim: true, set: (password) => {
+            if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)) {
+                return passwordEncrypt(password)
+            } else {
+                throw new Error("Password type is not correct.")
+            }
+        },
+    },
+
+    // image: { type: String, required: true, trim: true },
+},
+    { collection: 'users', timestamps: true })
+
+//? User Model Export
+module.exports = mongoose.model('User', userSchema)
