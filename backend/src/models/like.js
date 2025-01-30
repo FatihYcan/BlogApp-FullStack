101fc90aa1f502644f4b6d0d6373923a9a807bee
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-01-30 21:32:58 +0300

    Refactor like model to rename schema and improve code consistency

--- backend/src/models/like.js
+++ backend/src/models/like.js
@@ -1,22 +1,16 @@
 "use strict"
 
-/* --- BLOG API LIKE  --- */
+/* --- BLOG API COMMENT  --- */
 
-const { mongoose } = require('../configs/dbConnection');
+const { mongoose } = require('../configs/dbConnection')
 
 //? Like Model
-const LikeSchema = new mongoose.Schema({
-    blogId: { type: mongoose.Schema.Types.ObjectId, ref: 'Blog', required: true, index: true },
+const likeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+
+    blogId: { type: mongoose.Schema.Types.ObjectId, ref: 'Blog', required: true, index: true },
 },
-{ collection: 'likes', timestamps: true });
+    { collection: 'likes', timestamps: true })
 
 //? Like Model Export
-if (mongoose.models.Like) {
-    console.log('Like modeli zaten tanımlanmış');
-} else {
-    console.log('Like modeli yeni tanımlandı');
-}
-
-const Like = mongoose.model('Like', LikeSchema);
-module.exports = Like;
+module.exports = mongoose.model('Like', likeSchema)
