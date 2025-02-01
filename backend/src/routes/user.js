514dfa8f9cd24d17097cfa1cdc41e3eafe37f390
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-02-01 14:14:11 +0300

    Add image upload support to user routes; enable multiple image uploads for user creation and updates

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -6,9 +6,10 @@ const router = require('express').Router()
 
 const user = require('../controllers/user')
 const permissions = require('../middlewares/permissions')
+const upload = require('../middlewares/upload')
 
 //? URL: /users
-router.route('/').get(permissions.isAdmin, user.list).post(user.create)
-router.route('/:id').get(permissions.isLogin, user.read).put(permissions.isLogin, user.update).patch(permissions.isLogin, user.update).delete(permissions.isAdmin, user.delete)
+router.route('/').get(permissions.isAdmin, user.list).post(upload.array("images"), user.create)
+router.route('/:id').get(permissions.isLogin, user.read).put(permissions.isLogin, upload.array("images"), user.update).patch(permissions.isLogin, upload.array("images"), user.update).delete(permissions.isAdmin, user.delete)
 
 module.exports = router
