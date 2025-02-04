34184afeb1442aadfa9ed568fdde6d632e046533
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-02-04 13:05:12 +0300

    Comment out unused routes and components in AppRouter for cleaner code

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
-import NewBlog from "../pages/NewBlog";
-import About from "../pages/About";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import Detail from "../pages/Detail";
-import Register from "../pages/Register";
-import Login from "../pages/Login";
-import MyBlogs from "../pages/MyBlogs";
-import Profile from "../pages/Profile";
+// import NewBlog from "../pages/NewBlog";
+// import About from "../pages/About";
+// import Navbar from "../components/Navbar";
+// import Footer from "../components/Footer";
+// import Detail from "../pages/Detail";
+// import Register from "../pages/Register";
+// import Login from "../pages/Login";
+// import MyBlogs from "../pages/MyBlogs";
+// import Profile from "../pages/Profile";
 import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
@@ -17,15 +17,15 @@ const AppRouter = () => {
       {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="about" element={<About />} />
+        {/* <Route path="register" element={<Register />} /> */}
+        {/* <Route path="/login" element={<Login />} /> */}
+        {/* <Route path="about" element={<About />} /> */}
         <Route path="" element={<PrivateRouter />}>
-          <Route path="new-blog" element={<NewBlog />} />
+          {/* <Route path="new-blog" element={<NewBlog />} /> */}
         </Route>
-        <Route path="/detail/:_id" element={<Detail />} />
-        <Route path="my-blogs" element={<MyBlogs />} />
-        <Route path="profile" element={<Profile />} />
+        {/* <Route path="/detail/:_id" element={<Detail />} /> */}
+        {/* <Route path="my-blogs" element={<MyBlogs />} /> */}
+        {/* <Route path="profile" element={<Profile />} /> */}
       </Routes>
       {/* <Footer /> */}
     </Router>
