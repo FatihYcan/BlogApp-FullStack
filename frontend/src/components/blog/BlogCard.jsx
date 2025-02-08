fc462e97e13489696b972e32e3c22f8f9a061460
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-02-08 14:00:29 +0300

    Refactor BlogCard component for improved structure and styling; replace inline card rendering in MainContent with BlogCard component

--- frontend/src/components/blog/BlogCard.jsx
+++ frontend/src/components/blog/BlogCard.jsx
@@ -1,9 +1,168 @@
-import React from 'react'
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid2,
+  styled,
+  Typography,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 
-const BlogCard = () => {
+const SyledCard = styled(Card)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  padding: 0,
+  height: "100%",
+  backgroundColor: (theme.vars || theme).palette.background.paper,
+  "&:hover": {
+    backgroundColor: "transparent",
+    cursor: "pointer",
+  },
+  "&:focus-visible": {
+    outline: "3px solid",
+    outlineColor: "hsla(210, 98%, 48%, 0.5)",
+    outlineOffset: "2px",
+  },
+}));
+
+const SyledCardContent = styled(CardContent)({
+  display: "flex",
+  flexDirection: "column",
+  gap: 4,
+  padding: 16,
+  flexGrow: 1,
+  "&:last-child": {
+    paddingBottom: 16,
+  },
+});
+
+const StyledTypography = styled(Typography)({
+  display: "-webkit-box",
+  WebkitBoxOrient: "vertical",
+  WebkitLineClamp: 2,
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+});
+
+export default function BlogCard({
+  _id,
+  title,
+  image,
+  categoryId,
+  content,
+  likes,
+  comments,
+  views,
+  userId,
+  createdAt,
+}) {
   return (
-    <div>BlogCard</div>
-  )
-}
+    <Grid2 item xs={12} sm={6} md={4}>
+      <SyledCard variant="outlined">
+        <CardMedia
+          component="img"
+          alt={title}
+          image={image}
+          sx={{
+            aspectRatio: "16 / 9",
+            borderBottom: "1px solid",
+            borderColor: "divider",
+          }}
+        />
+        <SyledCardContent>
+          <Typography gutterBottom variant="caption" component="div">
+            {categoryId.name}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            {title}
+          </Typography>
+          <StyledTypography variant="body2" color="text.secondary" gutterBottom>
+            {content}
+          </StyledTypography>
+        </SyledCardContent>
 
-export default BlogCard
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+            alignItems: "center",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <FavoriteIcon sx={{ cursor: "pointer" }} />
+            <span style={{ fontSize: "1.2rem", marginLeft: "2px" }}>
+              {likes.length}
+            </span>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <ChatBubbleOutlineIcon sx={{ cursor: "pointer" }} />
+            <span style={{ fontSize: "1.2rem", marginLeft: "2px" }}>
+              {comments.length}
+            </span>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              cursor: "default",
+            }}
+          >
+            <VisibilityOutlinedIcon />
+            <span style={{ fontSize: "1.2rem", marginLeft: "2px" }}>
+              {views.length}
+            </span>
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 2,
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "16px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 1,
+              alignItems: "center",
+            }}
+          >
+            <AvatarGroup>
+              <Avatar
+                key={_id}
+                alt={userId.username}
+                src={userId.image}
+                sx={{ width: 24, height: 24 }}
+              />
+            </AvatarGroup>
+            <Typography variant="caption">{userId.username}</Typography>
+          </Box>
+          <Typography variant="caption">
+            {new Date(createdAt).toLocaleDateString("tr-TR")}
+          </Typography>
+        </Box>
+      </SyledCard>
+    </Grid2>
+  );
+}
--- frontend/src/pages/MainContent.jsx
+++ frontend/src/pages/MainContent.jsx
@@ -1,60 +1,16 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import AvatarGroup from "@mui/material/AvatarGroup";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Chip from "@mui/material/Chip";
-import Grid from "@mui/material/Grid2";
-import Typography from "@mui/material/Typography";
-import FormControl from "@mui/material/FormControl";
-import InputAdornment from "@mui/material/InputAdornment";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import { styled } from "@mui/material/styles";
+import {
+  Box,
+  Chip,
+  FormControl,
+  Grid2,
+  InputAdornment,
+  OutlinedInput,
+} from "@mui/material";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
 import useBlogCalls from "../hooks/useBlogCalls";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
-
-const SyledCard = styled(Card)(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  padding: 0,
-  height: "100%",
-  backgroundColor: (theme.vars || theme).palette.background.paper,
-  "&:hover": {
-    backgroundColor: "transparent",
-    cursor: "pointer",
-  },
-  "&:focus-visible": {
-    outline: "3px solid",
-    outlineColor: "hsla(210, 98%, 48%, 0.5)",
-    outlineOffset: "2px",
-  },
-}));
-
-const SyledCardContent = styled(CardContent)({
-  display: "flex",
-  flexDirection: "column",
-  gap: 4,
-  padding: 16,
-  flexGrow: 1,
-  "&:last-child": {
-    paddingBottom: 16,
-  },
-});
-
-const StyledTypography = styled(Typography)({
-  display: "-webkit-box",
-  WebkitBoxOrient: "vertical",
-  WebkitLineClamp: 2,
-  overflow: "hidden",
-  textOverflow: "ellipsis",
-});
+import { useEffect } from "react";
+import BlogCard from "../components/blog/BlogCard";
 
 export function Search() {
   return (
@@ -163,118 +119,11 @@ export default function MainContent() {
           <Search />
         </Box>
       </Box>
-      <Grid container rowSpacing={2} columnSpacing={2} justifyContent="center">
+      <Grid2 container rowSpacing={2} columnSpacing={2} justifyContent="center">
         {blogs.map((blog) => (
-          <Grid item xs={12} sm={6} md={4} key={blog._id}>
-            <SyledCard variant="outlined">
-              <CardMedia
-                component="img"
-                alt={blog.title}
-                image={blog.image}
-                sx={{
-                  aspectRatio: "16 / 9",
-                  borderBottom: "1px solid",
-                  borderColor: "divider",
-                }}
-              />
-              <SyledCardContent>
-                <Typography gutterBottom variant="caption" component="div">
-                  {blog.categoryId.name}
-                </Typography>
-                <Typography gutterBottom variant="h6" component="div">
-                  {blog.title}
-                </Typography>
-                <StyledTypography
-                  variant="body2"
-                  color="text.secondary"
-                  gutterBottom
-                >
-                  {blog.content}
-                </StyledTypography>
-              </SyledCardContent>
-
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: 2,
-                  alignItems: "center",
-                  justifyContent: "space-evenly",
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <FavoriteIcon sx={{ cursor: "pointer" }} />
-                  <span style={{ fontSize: "1.2rem", marginLeft: "2px" }}>
-                    {blog.likes.length}
-                  </span>
-                </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <ChatBubbleOutlineIcon sx={{ cursor: "pointer" }} />
-                  <span style={{ fontSize: "1.2rem", marginLeft: "2px" }}>
-                    {blog.comments.length}
-                  </span>
-                </Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    cursor: "default",
-                  }}
-                >
-                  <VisibilityOutlinedIcon />
-                  <span style={{ fontSize: "1.2rem", marginLeft: "2px" }}>
-                    {blog.views.length}
-                  </span>
-                </Box>
-              </Box>
-
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  gap: 2,
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  padding: "16px",
-                }}
-              >
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: 1,
-                    alignItems: "center",
-                  }}
-                >
-                  <AvatarGroup>
-                    <Avatar
-                      key={blog._id}
-                      alt={blog.userId.username}
-                      src={blog.userId.image}
-                      sx={{ width: 24, height: 24 }}
-                    />
-                  </AvatarGroup>
-                  <Typography variant="caption">
-                    {blog.userId.username}
-                  </Typography>
-                </Box>
-                <Typography variant="caption">
-                  {new Date(blog.createdAt).toLocaleDateString("tr-TR")}
-                </Typography>
-              </Box>
-            </SyledCard>
-          </Grid>
+          <BlogCard key={blog._id} {...blog} />
         ))}
-      </Grid>
+      </Grid2>
     </Box>
   );
 }
