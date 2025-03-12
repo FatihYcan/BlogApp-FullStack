f2eac15d2311a82072da0d8d5a8064ade1e2c08b
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-03-12 18:35:01 +0300

    refactor: remove unused warning notification function from ToastNotify helper

--- frontend/src/helper/ToastNotify.js
+++ frontend/src/helper/ToastNotify.js
@@ -1,16 +1,4 @@
 import { toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-
-export const toastWarnNotify = (msg) => {
-  toast.warn(msg, {
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  });
-};
 
 export const toastSuccessNotify = (msg) => {
   toast.success(msg, {
