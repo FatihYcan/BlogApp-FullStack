6c10470fc4462ccf3594034b97492c724132ee42
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-03-30 23:08:13 +0300

    fix: simplify mongoose connection options in dbConnection.js

--- backend/src/configs/dbConnection.js
+++ backend/src/configs/dbConnection.js
@@ -6,13 +6,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = function () {
     // Connect:
-    mongoose.connect(process.env.MONGODB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        poolSize: 10,
-        useFindAndModify: false,
-        useCreateIndex: true,
-    })
+    mongoose.connect(process.env.MONGODB) 
         .then(() => console.log('* DB Connected * '))
         .catch((err) => console.log('* DB Not Connected * ', err))
 }
