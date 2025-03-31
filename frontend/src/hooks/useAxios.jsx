482224432d1333d7e0eb2eb3faf304b35d49b79f
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-03-31 14:15:16 +0300

    fix: update environment variables and modify axios base URL for deployment

--- frontend/src/hooks/useAxios.jsx
+++ frontend/src/hooks/useAxios.jsx
@@ -4,25 +4,29 @@ const useAxios = () => {
   const userToken = localStorage.getItem("userToken") || {};
 
   const axiosPublic = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "/koseyazisi/",
   });
 
   const axiosData = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "/koseyazisi/",
     headers: {
       "Content-type": "multipart/form-data",
     },
   });
 
   const axiosWithToken = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "/koseyazisi/",
     headers: {
       Authorization: `Token ${userToken}`,
     },
   });
 
   const axiosWithTokenAndData = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "/koseyazisi/",
     headers: {
       Authorization: `Token ${userToken}`,
       "Content-type": "multipart/form-data",
