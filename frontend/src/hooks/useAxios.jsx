66fad5ca6afdcfb5ac1912c55d588c1b757e3440
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-04-01 22:09:40 +0300

    delete 1 file and update 1 file

--- frontend/src/hooks/useAxios.jsx
+++ frontend/src/hooks/useAxios.jsx
@@ -4,30 +4,26 @@ const useAxios = () => {
   const userToken = localStorage.getItem("userToken") || {};
 
   const axiosPublic = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
-    // baseURL: "https://koseyazisi.onrender.com/",
+    baseURL: "https://koseyazisi.onrender.com/",
   });
 
   const axiosData = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
-    // baseURL: "https://koseyazisi.onrender.com/",
+    baseURL: "https://koseyazisi.onrender.com/",
     headers: {
       "Content-type": "multipart/form-data",
     },
   });
 
   const axiosWithToken = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
-    // baseURL: "https://koseyazisi.onrender.com/",
-
+    baseURL: "https://koseyazisi.onrender.com/",
     headers: {
       Authorization: `Token ${userToken}`,
     },
   });
 
   const axiosWithTokenAndData = axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
-    // baseURL: "https://koseyazisi.onrender.com/",
+    // baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "https://koseyazisi.onrender.com/",
     headers: {
       Authorization: `Token ${userToken}`,
       "Content-type": "multipart/form-data",
