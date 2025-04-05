4c49320244dcafeb3c7829d0f9d3f8d06fa4e623
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-04-05 18:12:41 +0300

    update blog.js

--- backend/src/models/view.js
+++ backend/src/models/view.js
@@ -6,7 +6,6 @@ const { mongoose } = require('../configs/dbConnection')
 
 //? View Model
 const viewSchema = new mongoose.Schema({
-    
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
 
     blogId: { type: mongoose.Schema.Types.ObjectId, ref: 'Blog', required: true, index: true },
