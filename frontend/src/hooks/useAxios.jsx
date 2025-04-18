97021b322b7563892a891380b191bd9d4b46a699
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2025-04-18 16:47:23 +0300

    update useAxios.jsx

--- frontend/src/hooks/useAxios.jsx
+++ frontend/src/hooks/useAxios.jsx
@@ -6,9 +6,8 @@ const useAxios = () => {
   const deviceId = getDeviceId();
 
   const axiosPublic = axios.create({
-    // baseURL: "https://koseyazisi.onrender.com/",
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
-    headers: {
+    baseURL: "https://koseyazisi.onrender.com/",
+      headers: {
       ...(userToken
         ? { Authorization: `Token ${userToken}` }
         : { "x-device-id": deviceId }),
@@ -16,24 +15,21 @@ const useAxios = () => {
   });
 
   const axiosData = axios.create({
-    // baseURL: "https://koseyazisi.onrender.com/",
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "https://koseyazisi.onrender.com/",
     headers: {
       "Content-type": "multipart/form-data",
     },
   });
 
   const axiosWithToken = axios.create({
-    // baseURL: "https://koseyazisi.onrender.com/",
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "https://koseyazisi.onrender.com/",
     headers: {
       Authorization: `Token ${userToken}`,
     },
   });
 
   const axiosWithTokenAndData = axios.create({
-    // baseURL: "https://koseyazisi.onrender.com/",
-    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    baseURL: "https://koseyazisi.onrender.com/",
     headers: {
       Authorization: `Token ${userToken}`,
       "Content-type": "multipart/form-data",
